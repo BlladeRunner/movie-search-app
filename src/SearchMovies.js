74fcdcb79e5a0d6e0a592497761e5ccdffc8e355
Rufus JS <rufatahmedik@gmail.com>
2021-06-09 21:07:11 +0200

    style searchMovie component

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,18 +1,17 @@
 import React from "react"
+import "./style.css"
 
 export default function SearchMovies() {
     return (
-        <div>
-            <form className="form">
-                <label className="label" htmlFor="query">Movie Name</label>
-                    <input 
-                        className="input"
-                        type="text" 
-                        name="query"
-                        placeholder="i.e. Bladerunner 2049"
-                    />
-                    <button className="button" type="submit">Search</button>
-            </form>    
-        </div>
-    )    
+        <form className="form">
+            <label className="label" htmlFor="query">Movie Name</label>
+                <input 
+                    className="input"
+                    type="text" 
+                    name="query"
+                    placeholder="i.e. Bladerunner 2049"
+                />
+                <button className="button" type="submit">Search</button>
+        </form>
+    )      
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
-import SearchMovies from "./SearchMovies"
+import SearchMovies from "./SearchMovies";
 
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">Movie Search House</h1>
-        <SearchMovies />
+        <h1 className="title">Movie Search</h1>
+        <SearchMovies/>
       </div>
     );
   }
 }
 
 ReactDOM.render(<Main />, document.getElementById('root'));
-reportWebVitals();
