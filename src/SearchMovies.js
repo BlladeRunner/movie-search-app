2e030afc5424055b36e5c3cf4d7bd0f23018bc46
Rufus JS <rufatahmedik@gmail.com>
2021-06-10 16:44:50 +0200

    adding API

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -2,6 +2,23 @@ import React from "react"
 import "./style.css"
 
 export default function SearchMovies() {
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+        
+        const query = "Bladerunner 2049";
+        
+        const url = "https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false";
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
     return (
         <form className="form">
             <label className="label" htmlFor="query">Movie Name</label>
