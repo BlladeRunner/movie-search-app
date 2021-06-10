9508de84f1a7e24d931e638c988a03df1359837f
Rufus JS <rufatahmedik@gmail.com>
2021-06-10 17:19:13 +0200

    adding new API

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -8,7 +8,7 @@ export default function SearchMovies() {
         
         const query = "Bladerunner 2049";
         
-        const url = "https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false";
+        const url = "https://api.themoviedb.org/3/movie/550?api_key=bea0e997a9ff807e35f4e2eaffdedaa7";
         
         try {
             const res = await fetch(url);
